ec893d519a01a2041f87dfe4f419a8f6f7513bfd
paweleck <czemierysp@op.pl>
2017-02-27 12:24:36 +0100

    Building a Dynamic UI with Fragments

--- app/src/main/java/com/pawel/testappandroid/MainActivity.java
+++ app/src/main/java/com/pawel/testappandroid/MainActivity.java
@@ -1,10 +1,14 @@
 package com.pawel.testappandroid;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.pawel.testappandroid.fragments.ArticleFragment;
+import com.pawel.testappandroid.fragments.HeadlinesFragment;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.pawel.testappandroid.MESSAGE";
@@ -13,6 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            HeadlinesFragment firstFragment = new HeadlinesFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
     }
 
     /** Called when the user clicks the Send button
@@ -24,5 +51,23 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void replaceFrag(View view) {
+        // Create fragment and give it an argument specifying the article it should show
+        ArticleFragment newFragment = new ArticleFragment();
+//        Bundle args = new Bundle();
+//        args.putInt(ArticleFragment.ARG_POSITION, position);
+//        newFragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, newFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
     }
 }
